a9d98d5a7510269d9431ab9c38a2c40486e91b6c
3xtZzy <38046904+VanQube@users.noreply.github.com>
2021-02-28 20:36:06 +0300

    feat: add solution to task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 
 // You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+    let new_arr = []
+    for (let elem in matrix) {
+        if (elem%2==0) {
+            new_arr = new_arr.concat(matrix[elem])
+        } else {
+            let odd = [];
+            odd += matrix[elem]
+            odd = odd.split(',').reverse();
+            odd = odd.map(elem => +elem)
+            new_arr = new_arr.concat(odd);
+        }
+    } return new_arr
 }
